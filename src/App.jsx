fd8ff078d27220524f62d28652cb4c9f1f7a1a9b
Pedro Germano <pedrogermano232@gmail.com>
2024-01-13 16:31:26 -0300

    focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import star from "./assets/star.svg";
+import search from "./assets/search.svg";
+
 function App() {
   return (
     <div className="card">
+      <header>
+        <h2>What others think about the product</h2>
+        <div className="summary">
+          <div>
+            <img src={star} alt="star icon" />
+            <span>4.8</span>
+          </div>
+          <p>Average customer rating</p>
+        </div>
+      </header>
 
+      <form className="search">
+        <input
+          type="search"
+          name="review-search"
+          id="review-search"
+          placeholder="Search topics and reviews"
+        />
+        <button type="submit" aria-label="Run search">
+          <img src={search} alt="search icon" />
+        </button>
+      </form>
+      <div className="reviews">
+        <p>
+          <strong>Reviews</strong>
+        </p>
+        <div className="reviews-container">106</div>
+      </div>
+      <button type="button" className="btn">
+        Write a review
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
